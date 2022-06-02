eb82aae63f7e0c8e40556f7353da8e2ef456cc94
Murthy <naaveen2@gmail.com>
2022-06-02 12:45:38 +0530

    Fixed profile uploading

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { base64ToFile } from 'ngx-image-cropper';
+import { base64ToFile, ImageCroppedEvent } from 'ngx-image-cropper';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { UtilService } from 'src/app/services/util.service';
 
@@ -10,14 +10,16 @@ import { UtilService } from 'src/app/services/util.service';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  userDetails: Object |any;
+  userDetails: Object | any;
   profilePic: any = '';
-  profilePicModel:boolean = false;
+  profilePicModel: boolean = false;
   profileImage: boolean = false;
 
-  constructor(private authService: AuthenticationService,
+  constructor(
+    private authService: AuthenticationService,
     private utilService: UtilService,
-    private sanitizer : DomSanitizer) {}
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
     this.getProfileDetails();
@@ -25,53 +27,54 @@ export class ProfileComponent implements OnInit {
 
   getProfileDetails() {
     this.utilService.showLoader();
-    this.authService.profile().subscribe((res: any) => {
-      this.utilService.hideLoader();
-      if (res.status) {
-        this.userDetails = res.user;
-        if(res.user.profile_pic){
-          this.profileImage = true;
+    this.authService.profile().subscribe(
+      (res: any) => {
+        this.utilService.hideLoader();
+        if (res.status) {
+          this.userDetails = res.user;
+          if (res.user.profile_pic) {
+            this.profileImage = true;
+            res.user.profile_pic = this.sanatizeUrl(res.user.profile_pic);
+          } else {
+            this.profileImage = false;
+          }
         } else {
-          this.profileImage = false;
+          this.userDetails = '';
         }
-      } else{
-        this.userDetails = '';
+      },
+      (err: any) => {
+        console.log(err);
+        return false;
       }
-    },
-    (err:any)=>{
-      console.log(err);
-      return false;
-    });
+    );
   }
 
-  setProfilePic(){
+  setProfilePic() {
     var imageBlob: Blob = base64ToFile(this.profilePic);
     var imageName: string = this.userDetails.name.toLowerCase().toString();
     var imageFile: File = new File([imageBlob], imageName, {
-      type: "image/jpeg"
+      type: 'image/jpeg',
     });
     var blobImage = window.URL.createObjectURL(imageFile);
     // Opens image in new tab
     // window.open(blobImage);
     var body = {
       email: this.userDetails.email,
-      profile_pic: blobImage,
+      profile_pic: this.profilePic,
     };
     this.utilService.showLoader();
-    this.authService.updateProfile(body).subscribe(
-      (res:any)=>{
-        this.utilService.hideLoader();
-        this.profilePicModel = !this.profilePicModel;
-        this.getProfileDetails();
-      }
-    )
+    this.authService.updateProfile(body).subscribe((res: any) => {
+      this.utilService.hideLoader();
+      this.profilePicModel = !this.profilePicModel;
+      this.getProfileDetails();
+    });
   }
 
-  sanatizeUrl(generatedImageUrl:any): SafeResourceUrl {
+  sanatizeUrl(generatedImageUrl: any): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(generatedImageUrl);
   }
 
-  onImgError(){
+  onImgError() {
     this.profileImage = false;
   }
 }
